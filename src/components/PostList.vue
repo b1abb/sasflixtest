f9492e85e03dd4f9c26390fbd2dfae6e2eebcfb0
b1abb <blabb.a@ya.ru>
2024-10-29 00:11:13 +0300

    загрузка постов

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <PostItem v-for="post in posts" :key="post.id" :post="post" />
+    <PostItem v-if="!loading" v-for="post in posts" :key="post.id" :post="post" />
+    <LoadingSpinner v-if="loading" />
   </div>
 </template>
 
@@ -8,10 +9,23 @@
 import { usePosts } from '~/composables/postApi.ts';
 import { onMounted } from 'vue';
 import PostItem from './PostItem.vue';
+import LoadingSpinner from "~/components/LoadingSpinner.vue";
 
+const loading = ref(true);
 const { posts, fetchPosts } = usePosts();
 
+async function loadComments() {
+  try {
+    loading.value = true;
+    await fetchPosts();
+  } catch (error) {
+    console.error("Failed to fetch comments:", error);
+  } finally {
+    loading.value = false;
+  }
+}
+
 onMounted(() => {
-  fetchPosts();
+  loadComments();
 });
 </script>
